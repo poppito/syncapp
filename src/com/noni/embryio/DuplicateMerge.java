e0a5b9c52e72daa308aa7bbed9b0766e8654320d
Harsh O <harsh@embry.io>
2015-03-24 21:27:36 +1100

    - getting closer with merging
    - some issues with downloadable contacts :(
    - getting merge to work with async task

--- src/com/noni/embryio/DuplicateMerge.java
+++ src/com/noni/embryio/DuplicateMerge.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract.RawContacts;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -32,12 +34,40 @@ public class DuplicateMerge extends FragmentActivity {
 		  mergeContacts.setAdapter(mergeArrayAdapter);
 		  mergeContacts.setChoiceMode(mergeContacts.CHOICE_MODE_MULTIPLE);
 		  Log.v(TAG, mergeDuplicates.toString() + " " + mergeSyncedContacts.toString());
+		  cr = getContentResolver();
+		  findAllDuplicates(cr, mergeDuplicates);
 	  }
 	  
 	  
-	  public void DuplicateMerge()
+	  
+	  public void findAllDuplicates(ContentResolver cr, ArrayList<String> duplicateContacts)
 	  {
-		  
+		  ArrayList<String> mergeDuplicates;
+		  this.cr = cr;
+		  this.mergeDuplicates = duplicateContacts;
+		  String name, accountName = null; String accountType = null;
+		  String[] proj = {RawContacts.DISPLAY_NAME_PRIMARY, RawContacts.CONTACT_ID, RawContacts.ACCOUNT_NAME, RawContacts.ACCOUNT_TYPE, RawContacts.DELETED};
+		  Cursor C = cr.query(RawContacts.CONTENT_URI, proj, null, null, null);
+
+			while (C.moveToNext())
+			{
+				name = C.getString(C.getColumnIndex(RawContacts.DISPLAY_NAME_PRIMARY));
+				int deleted = C.getInt(C.getColumnIndex(RawContacts.DELETED));
+				if (deleted != 1)
+				{
+					Log.v(TAG, "name is " + name);
+				
+				
+					if ( (C.getString(C.getColumnIndex(RawContacts.ACCOUNT_NAME)) != null) 
+							&& (C.getString(C.getColumnIndex(RawContacts.ACCOUNT_TYPE)) != null) )
+					{
+							accountName = C.getString(C.getColumnIndex(RawContacts.ACCOUNT_NAME));
+							accountType = C.getString(C.getColumnIndex(RawContacts.ACCOUNT_TYPE));
+							
+							Log.v(TAG, "account name is " + accountName.toString() + " account type " + accountType.toString());
+					}
+				}
+			}
 	  }
 	
 }
